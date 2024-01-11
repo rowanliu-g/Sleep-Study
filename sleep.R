44b48425d35873eaa16b16830b931eae45207786
Rowan Liu <gejunhui.liu05@gmail.com>
2024-01-11 12:15:27 -0500

    Changes to sleep.R

--- sleep.R
+++ sleep.R
@@ -1,6 +1,12 @@
-library(ggplot2)
-ggplot(data=mpg) +
-  geom_point(mapping = aes(x = cty,
-                           y = hwy,
-                           color = class))
+getwd()
+setwd("/Users/17739/Sleep-Study")
 
+library(viridis)
+library(ggplot2)
+library(gridExtra)
+
+#### DATA ####
+sleep <- read.csv('https://github.com/rowanliu-g/Sleep-Study/blob/main/Sleep_health_and_lifestyle_dataset.csv')
+class(sleep)
+dim(sleep)
+head(sleep)
