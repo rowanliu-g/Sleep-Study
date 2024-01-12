5258f5c2f9b62086d41f25114f64ace623abc51a
Rowan Liu <gejunhui.liu05@gmail.com>
2024-01-12 10:47:24 -0500

    Changes to sleep.R

--- sleep.R
+++ sleep.R
@@ -1,3 +1,4 @@
+# How might physical activity affect stress and sleep duration?
 getwd()
 setwd("/Users/17739/Sleep-Study")
 
@@ -6,7 +7,21 @@ library(ggplot2)
 library(gridExtra)
 
 #### DATA ####
-sleep <- read.csv('https://github.com/rowanliu-g/Sleep-Study/blob/main/Sleep_health_and_lifestyle_dataset.csv')
+sleep <- read.csv('C:/Users/17739/Sleep-Study/Sleep_health_and_lifestyle_dataset.csv')
 class(sleep)
 dim(sleep)
-head(sleep)
+head(sleep) 
+
+#### PLOT ####
+phys <- sleep[,c(7)]
+dur <- sleep[,c(5)]
+occup <- sleep[,c(4)]
+
+plt4 <- ggplot(data = sleep) + 
+  geom_point(mapping = aes(x= phys, y=dur, color= occup)) +
+  scale_color_viridis(discrete = TRUE, option = "turbo") +
+  labs(title = "Sleep and Exercise based on Occupation", x = "Physical Activity (min)",
+       y = "Sleep Duration (hr)", color = "Occupation")
+
+plt4
+
